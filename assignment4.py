780a3cd837d86395fdfbf41de7cf6874d192332c
Junaid Mukhtar <junaidmukhtar152@gmail.com>
2026-06-11 23:53:35 +0500

    Completed lecture 4

---
+++ assignment4.py
@@ -0,0 +1,16 @@
+# Assignment 4 
+marks = 85
+
+# Conditions check karne ke liye if-elif-else statement ka use kiya
+if marks == 100:
+    print("Topper")
+elif marks > 90:
+    print("A+ Grade")
+elif marks > 80:
+    print("A Grade")
+elif marks > 70:
+    print("B Grade")
+elif marks > 40:
+    print("Pass")
+else:
+    print("Fail")  # Agar marks 40 ya usse kam hain
