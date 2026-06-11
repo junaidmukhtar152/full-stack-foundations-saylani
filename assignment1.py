76496fa8ba9a3439392c87a7e66f53b559e651ce
Junaid Mukhtar <junaidmukhtar152@gmail.com>
2026-06-11 15:02:49 +0500

    Completed lecture 2

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+#Task 1: Create Variables
+my_name = "Junaid Mukhtar"
+my_age = 23
+my_city = "Faisalabad"
+isStudent = True
+
+print(my_name)
+print(my_age)
+print(my_city)
+print(isStudent)
+
+
+#Task 2: Check Data Types
+
+name = "Ali"
+age = 18
+isStudent = True
+
+print(type(name))
+print(type(age))
+print(type(isStudent))
+
+
