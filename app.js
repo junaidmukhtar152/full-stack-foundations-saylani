f85e81d4430187eab4e6dc4426b7df20bbccc205
Junaid Mukhtar <junaidmukhtar152@gmail.com>
2026-06-11 12:37:10 +0500

    First assignment completed

---
+++ app.js
@@ -0,0 +1,5 @@
+
+
+console.log("Name: Junaid Mukhtar");
+console.log("Age: 23");
+console.log("Favorite Programming Language: JavaScript");
---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("Name: Junaid Mukhtar")
+print("Age: 23")
+print("City: Faisalabad")
+print("Favorite Hobby: Learning Web Development")
