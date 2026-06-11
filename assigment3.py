1fd0a3236dd49c2d6eeafbf5f319717149e9d025
Junaid Mukhtar <junaidmukhtar152@gmail.com>
2026-06-11 15:22:05 +0500

    Completed lecture 3

---
+++ assigment3.py
@@ -0,0 +1,45 @@
+# Task 1: Print Statements
+
+print("My Name is Junaid Mukhtar")
+print("I am learning Python")
+print("Python is fun!")
+
+# Task 2: Variables
+
+name = "Junaid Mukhtar"
+age = 21
+city = "Faisalabad"
+
+print(name)
+print(age)
+print(city)
+
+# Task 3: Addition
+
+num1 = 15
+num2 = 10
+
+print(num1 + num2)
+
+# Task 4: Multiplication
+
+a = 8
+b = 5
+
+print(a * b)
+
+# Task 5: Operators Practice
+
+print(20 + 10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+print(20 % 3)
+print(20 // 3)
+print(2 ** 4)
+
+# Bonus Task
+
+favorite_number = 7
+
+print("My favorite number is", favorite_number)
